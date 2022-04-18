8e79de519b10b52eef1e6e89ba26334889ae56d4
Tony Zhuo <tonyzhuo@25sprout.com>
2022-04-18 16:06:28 +0800

    style: adjust exchangeRateService ratio json

--- app/Services/ExchangeRateService.php
+++ app/Services/ExchangeRateService.php
@@ -39,9 +39,33 @@ class ExchangeRateService
      * 
      * @return object 各幣別匯率
      */
-    private function ratio(): object
+    public function ratio(): object
     {
-        $ratioObject = json_decode('{"currencies": {"TWD": {"TWD": 1,"JPY": 3.669,"USD": 0.03281},"JPY": {"TWD": 0.26956,"JPY": 1,"USD": 0.00885},"USD": {"TWD": 30.444,"JPY": 111.801,"USD": 1}}}');
+        $ratioObject = json_decode('
+        {
+            "currencies": 
+            {
+                "TWD": 
+                {
+                    "TWD": 1,
+                    "JPY": 3.669,
+                    "USD": 0.03281
+                },
+                "JPY": 
+                {
+                    "TWD": 0.26956,
+                    "JPY": 1,
+                    "USD": 0.00885
+                },
+                "USD": 
+                {
+                    "TWD": 30.444,
+                    "JPY": 111.801,
+                    "USD": 1
+                }
+            }
+        }');
+
         return $ratioObject->currencies;
     }
 }
